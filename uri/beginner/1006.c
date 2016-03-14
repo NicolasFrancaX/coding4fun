83e9d91fabf833e76a67036ff979771309d273bb
Nicolas França <nicolasfranca9@gmail.com>
2016-03-14 12:14:20 -0300

    Read three values (variables A, B and C), which are the three student's grades. Then, calculate the average, considering that grade A has weight 2, grade B has weight 3 and the grade C has weight 5. Consider that each grade can go from 0 to 10.0, always with one decimal place.

---
+++ uri/beginner/1006.c
@@ -0,0 +1,17 @@
+/*
+ * Problem: https://www.urionlinejudge.com.br/judge/en/problems/view/1006
+ *
+ */
+
+#include <stdio.h>
+
+int main() {
+    double A, B, C, MEDIA;
+
+    scanf("%lf", &A);
+    scanf("%lf", &B);
+    scanf("%lf", &C);
+
+    MEDIA = (0.2 * A) + (0.3 * B) + (0.5 * C);
+    printf("MEDIA = %.1lf\n", MEDIA);
+}
