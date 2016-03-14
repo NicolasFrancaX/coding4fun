b306f8d5a25f92f720f749f411f3371833d4cdfc
Nicolas França <nicolasfranca9@gmail.com>
2016-03-13 22:15:12 -0300

    The formula to calculate the area of a circumference is defined as A = π . R2. Considering to this problem that π = 3.14159:

---
+++ uri/beginner/1002.c
@@ -0,0 +1,15 @@
+/*
+ * Problem: https://www.urionlinejudge.com.br/judge/en/problems/view/1002
+ *
+ */
+
+#include <stdio.h>
+
+int main() {
+    double A, R;
+
+    scanf("%lf", &R);
+
+    A = 3.14159 * R * R;
+    printf("A=%.4lf\n", A);
+}
