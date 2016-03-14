d508b5f12a4505af820ded8b430a2de3cc6db0cf
Nicolas França <nicolasfranca9@gmail.com>
2016-03-13 22:16:28 -0300

    Read two integer values. After this, calculate the product between them and store the result in a variable named PROD. Print the result like the example below. Do not forget to print the end of line after the result, otherwise you will receive “Presentation Error”.

---
+++ uri/beginner/1004.c
@@ -0,0 +1,16 @@
+/*
+ * Problem: https://www.urionlinejudge.com.br/judge/en/problems/view/1004
+ *
+ */
+
+#include <stdio.h>
+
+int main() {
+    int a, b, PROD;
+
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    PROD = a * b;
+    printf("PROD = %d\n", PROD);
+}
