d37d052faac1b75e589b8b0d2a1efb4cf6362450
Nicolas França <nicolasfranca9@gmail.com>
2016-03-13 22:15:58 -0300

    Read two integer values, in this case, the variables A and B. After this, calculate the sum between them and assign it to the variable SOMA. Write the value of this variable.

---
+++ uri/beginner/1003.c
@@ -0,0 +1,16 @@
+/*
+ * Problem: https://www.urionlinejudge.com.br/judge/en/problems/view/1003
+ *
+ */
+
+#include <stdio.h>
+
+int main() {
+    int A, B, SOMA;
+
+    scanf("%d", &A);
+    scanf("%d", &B);
+
+    SOMA = A + B;
+    printf("SOMA = %d\n", SOMA);
+}
