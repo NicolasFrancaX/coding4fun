ba3b815469ad119f49a1c0d3b42eec3c6570404e
Nicolas França <nicolasfranca9@gmail.com>
2016-03-18 16:00:49 -0300

    You must make a program that read a float-point number and print a message saying in which of following intervals the number

---
+++ uri/beginner/1037.c
@@ -0,0 +1,26 @@
+/*
+ * Problem: https://www.urionlinejudge.com.br/judge/en/problems/view/1037
+ *
+ */
+
+#include <stdio.h>
+
+int main() {
+    float a;
+
+    scanf("%f", &a);
+
+    if(a >= 0 && a <= 25) {
+        printf("Intervalo [0,25]\n");
+    } else if(a > 25 && a <= 50) {
+        printf("Intervalo (25,50]\n");
+    } else if(a > 50 && a <= 75) {
+        printf("Intervalo (50,75]\n");
+    } else if(a > 75 && a <= 100) {
+        printf("Intervalo (75,100]\n");
+    } else {
+        printf("Fora de intervalo\n");
+    }
+
+    return 0;
+}
