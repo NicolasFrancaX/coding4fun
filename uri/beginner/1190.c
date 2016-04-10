102db0a18c323f9b08a9a28f6a871d0ef65a3921
Nicolas França <nicolasfranca9@gmail.com>
2016-04-10 19:12:55 -0300

    Read an uppercase character that indicates an operation that will be performed in an array M[12][12]. Then, calculate and print the sum or average considering only that numbers that are included in the right area (green area) of this array, like shown in the following figure.

---
+++ uri/beginner/1190.c
@@ -0,0 +1,33 @@
+/*
+ * Problem: https://www.urionlinejudge.com.br/judge/en/problems/view/1190
+ *
+ */
+
+#include <stdio.h>
+
+int main() {
+    double n[12][12], sum = 0, average = 0;
+    int i, j;
+    char operation;
+
+    scanf("%c", &operation);
+
+    for(i = 0; i < 12; i++) {
+        for(j = 0; j < 12 ; j++) {
+            scanf("%lf", &n[i][j]);
+            if(i + j > 11 && i < j) {
+                sum += n[i][j];
+            }
+        }
+    }
+
+    if(operation == 'S') {
+        printf("%.1lf\n", sum);
+
+    } else if(operation, 'M') {
+        average = (sum + 0.0) / 30;
+        printf("%.1lf\n", average);
+    }
+
+    return 0;
+}
