fe5af486ae6918b7d780495505b77b305907c889
Nicolas França <nicolasfranca9@gmail.com>
2018-08-07 11:23:30 -0300

    URI 1001 em C++

---
+++ uri/C++/beginner/1001/1001.cpp
@@ -0,0 +1,14 @@
+// Problem: https://www.urionlinejudge.com.br/judge/en/problems/view/1001
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int A, B;
+	while (cin >> A) {
+		cin >> B;
+		cout << "X = " << A + B << endl;
+	}
+
+	return 0;
+}
