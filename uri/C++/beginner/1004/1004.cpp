9215fdc76967b5602f4778bd3a206545f0da661b
Nicolas França <nicolasfranca9@gmail.com>
2018-08-13 20:09:31 -0300

    uri 1004 c++

---
+++ uri/C++/beginner/1004/1004.cpp
@@ -0,0 +1,14 @@
+// Problem: https://www.urionlinejudge.com.br/judge/en/problems/view/1004
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int A, B;
+	while (cin >> A) {
+		cin >> B;
+
+		cout << "PROD = " << A*B << endl;
+	}
+}
